47b74fd16985d622c80d56d51f04ddaf75bc3caa
Vicky Rana <vickyrana4433@gmail.com>
2021-01-06 00:59:31 +0500

    api

--- app/Review.php
+++ app/Review.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $appends =['created_at_format'];
     protected $fillable = ['user_id', 'product_id', 'order_id', 'rating', 'comment'];
 
     public function product()
@@ -16,5 +18,10 @@ class Review extends Model
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function getCreatedAtFormatAttribute()
+    {
+        return Carbon::parse($this->created_at)->diffForHumans();
     }
 }
