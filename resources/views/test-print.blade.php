681e1fb443576f0f5ae40838f73831f3108d3e4a
moizchauhdry <moizchauhdry@gmail.com>
2021-02-08 22:36:21 +0500

    Test printing

--- resources/views/test-print.blade.php
+++ resources/views/test-print.blade.php
@@ -12,22 +12,22 @@
     <?php
         // use Mike42\Escpos\PrintConnectors\FilePrintConnector;
         // use Mike42\Escpos\Printer;
-        // $connector = new FilePrintConnector("php://stdout");
-        // $printer = new Printer($connector);
-        // $printer -> text("SWAAD!\n");
+        // $connector = new FilePrintConnector("LPT1");
+        // $printer = new Escpos($connector);
+        // $printer -> text("Hello World\n");
         // $printer -> cut();
         // $printer -> close();
         // dd('FilePrinting');
 
         use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
         use Mike42\Escpos\CapabilityProfile;
-        $profile = CapabilityProfile::load("SP2000");
-        $connector = new WindowsPrintConnector("smb://computer/printers");
-        dd($connector);
-        $printer = new Printer($connector, $profile);
-        $printer -> text("SWAAD!\n");
-        $printer -> cut();
-        $printer -> close();
+        // $profile = CapabilityProfile::load("SP2000");
+        $connector = new WindowsPrintConnector("EPSON TM-T88V");
+        // dd($connector);
+        $printer = new Printer($connector);
+        $printer->text("SWAAD!\n");
+        $printer->cut();
+        $printer->close();
         dd('WindowsPrinting');
 
     ?>
