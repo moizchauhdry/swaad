aff4f38d003ff7a7471285a30071cee3f82cd462
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2022-07-13 23:47:44 +0500

    # footer text change

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -12,7 +12,16 @@
                 <div class="ftco-footer-widget mb-4">
                     <h2 class="ftco-heading-2">SWAAD</h2>
                     <p class="text-justify">
-                        {{session('lan') == 'en' ? 'Swaad is our passion and we passionately welcome you in our restaurant with unmatched taste and variety. We use fresh vegetables from the region, our meat comes from Berns oldest butscher shop (Spahni AG), herbs and spices from Pakistan and India, tea from Himalaya region and long grain basmati rice from the fields of Punjab and pink Himalaya Salt from the world’s biggest salt mine in Pothohar range of Pakistan.' : 'Swaad ist unsere Leidenschaft und wir begrüßen Sie leidenschaftlich in unserem Restaurant mit unvergleichlichem Geschmack und Abwechslung. Wir verwenden frisches Gemüse aus der Region, unser Fleisch stammt aus Berns ältestem Butscherladen (Spahni AG), Kräutern und Gewürzen aus Pakistan und Indien, Tee aus der Himalaya-Region und langkörnigem Basmatireis aus den Feldern Punjab und rosa Himalaya-Salz aus der Welt größtes Salzbergwerk in Pothohar in Pakistan.'}}
+                        {{session('lan') == 'en' ? 'Swaad is our passion and we passionately welcome you in our
+                        restaurant with unmatched taste and variety. We use fresh vegetables, our meat
+                        comes from Berns oldest butscher shop (Spahni AG), herbs and spices from Pakistan and India, tea
+                        from Himalaya region and long grain basmati rice from the fields of Punjab and pink Himalaya
+                        Salt from the world’s biggest salt mine in Pothohar range of Pakistan.' : 'Swaad ist unsere
+                        Leidenschaft und wir begrüßen Sie leidenschaftlich in unserem Restaurant mit unvergleichlichem
+                        Geschmack und Abwechslung. Wir verwenden frisches Gemüse, unser Fleisch stammt
+                        aus Berns ältestem Butscherladen (Spahni AG), Kräutern und Gewürzen aus Pakistan und Indien, Tee
+                        aus der Himalaya-Region und langkörnigem Basmatireis aus den Feldern Punjab und rosa
+                        Himalaya-Salz aus der Welt größtes Salzbergwerk in Pothohar in Pakistan.'}}
                     </p>
                     <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
                         <li class="ftco-animate"><a href="https://fb.me/swaadbern" target="_blank"><span
@@ -28,17 +37,17 @@
                     </h2>
                     <div class="d-flex">
                         <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
-                            <li><a href="{{route('about')}}"
-                                    class="py-2 d-block">{{session('lan') == 'en' ? 'About us' : 'Über uns'}}</a>
+                            <li><a href="{{route('about')}}" class="py-2 d-block">{{session('lan') == 'en' ? 'About us'
+                                    : 'Über uns'}}</a>
                             </li>
-                            <li><a href="{{route('serve')}}"
-                                    class="py-2 d-block">{{session('lan') == 'en' ? 'What We Serve' : 'Was wir servieren'}}</a>
+                            <li><a href="{{route('serve')}}" class="py-2 d-block">{{session('lan') == 'en' ? 'What We
+                                    Serve' : 'Was wir servieren'}}</a>
                             </li>
-                            <li><a href="{{route('termsCondition')}}"
-                                    class="py-2 d-block">{{session('lan') == 'en' ? 'Terms & Conditions' : 'Terms & Bedingungen'}}</a>
+                            <li><a href="{{route('termsCondition')}}" class="py-2 d-block">{{session('lan') == 'en' ?
+                                    'Terms & Conditions' : 'Terms & Bedingungen'}}</a>
                             </li>
-                            <li><a href="{{route('privacy')}}"
-                                    class="py-2 d-block">{{session('lan') == 'en' ? 'Privacy Policy' : 'Datenschutz-Bestimmungen'}}</a>
+                            <li><a href="{{route('privacy')}}" class="py-2 d-block">{{session('lan') == 'en' ? 'Privacy
+                                    Policy' : 'Datenschutz-Bestimmungen'}}</a>
                             </li>
                         </ul>
                     </div>
@@ -77,4 +86,5 @@
 <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
         <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
         <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
-            stroke="#F96D00" /></svg></div>
+            stroke="#F96D00" />
+    </svg></div>
